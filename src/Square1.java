6d89cc4d21c6ff2cbf9e5d6d30787385febab31a
Vanshika Dhyani <vanshikadhyani11@gmail.com>
2021-06-23 23:25:05 +0100

    Remove space

--- src/Square1.java
+++ src/Square1.java
@@ -4,7 +4,6 @@ public class Square1 {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
-
         for (int i = 1; i <=n ; i++) {
             for (int j = 1; j <= n; j++) {
                 System.out.print(i);
